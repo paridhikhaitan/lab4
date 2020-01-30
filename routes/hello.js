82556fe6e04b602db8066603b973caf38f8ae2cc
Paridhi Khaitan <paridhi4699@gmail.com>
2020-01-30 12:30:28 -0800

    completed lab

--- routes/hello.js
+++ routes/hello.js
@@ -4,7 +4,8 @@
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+  var nameToShow = req.params.username;
+  res.render('hello', {
+  	'name': nameToShow,
   });
 };
